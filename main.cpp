53c200606e2ccc20bf6439e3b61cbb922791f205
dekel241 <dekel241@gmail.com>
2021-06-25 12:13:02 +0300

    Refactor main

--- main.cpp
+++ main.cpp
@@ -1,28 +1,28 @@
 #include "scanner.h"
 extern Token_stream ts;
 #include <iostream>
-int main()
-try
-{
-    int val = 0;
-    cout << "> "; // prompt
-    while (cin) {
-        Token t = ts.get();
-        if (t.kind == 'q') {
-            cout << "quit\n";
-            break;    // quit calc
-        }
-        if (t.kind == EOL) {         // print result
-            cout << "= " << val << '\n';
-            cout << "> "; // prompt
-        }
-        else {
-            ts.putback(t);
-            val = statement();
+
+int main(void) {
+    try {
+        int val = 0;
+        cout << "> "; // prompt
+        while (cin) {
+            Token t = ts.get();
+            if (t.kind == 'q') {
+                cout << "quit\n";
+                break;    // quit calc
+            }
+            if (t.kind == EOL) {         // print result
+                cout << "= " << val << '\n';
+                cout << "> "; // prompt
+            } else {
+                ts.putback(t);
+                val = statement();
+            }
         }
     }
-}
-catch (exception& e) {
-    cerr << "Error, " << e.what() << '\n';
-    return 1;
+    catch (exception& e) {
+        cerr << "Error, " << e.what() << '\n';
+        return 1;
+    }
 }
